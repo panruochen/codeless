37eee261b382147318fc07c069ad139d4e8c25ce
Pan Ruochen <ijkxyz@msn.com>
2016-09-16 11:01:37 +0800

    Remove unused files and rules

--- server/config.mk
+++ server/config.mk
@@ -4,16 +4,13 @@ EXTRA           += -Wall ##-Wno-unused-result #-Wno-format
 DEFINES         := -DSANITY_CHECK
 SRCS            := $(WD)/
 INCS            := $(SRCS) ./common/
-TARGET          := bin/cl-server.exe
+TARGET          := cl-server.exe
 LIBS            := -lpthread
 OBJ_DIR         := .objs-server
 SRC_EXTS        := cpp c
 #MY_ASM_EXTS     := asm
 EXCLUDE_FILES   := common/got_error.cpp
 DEPENDENT_LIBS  := lib/libcommon.a
-#OVERRIDE_CMD_AS := tools/nasm -f coff -o $$@ $$<
-OVERRIDE_CMD_AS := tools/ml -nologo -c -Fo$$@ $$<
-CFLAGS_server/cl-server.cpp := -DMOON
 
 $(call BUILD_EXECUTABLE)
 $(call CLEAN_VARS)
